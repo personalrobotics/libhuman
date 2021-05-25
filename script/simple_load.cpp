79ba2f8b45fb9011ab8c0a8db5209e9dc9dd501c
sniyaz <sniyaz@berkeley.edu>
2021-05-25 16:59:08 -0700

    Load table in simple_load. In wrong place.

--- script/simple_load.cpp
+++ script/simple_load.cpp
@@ -11,10 +11,29 @@
 static const std::string topicName("dart_markers");
 static const std::string baseFrameName("map");
 
+using dart::dynamics::SkeletonPtr;
+
 void waitForUser(const std::string& msg)
 {
   std::cout << msg;
   std::cin.get();
+}
+
+const SkeletonPtr makeBodyFromURDF(
+    const std::shared_ptr<aikido::io::CatkinResourceRetriever>
+        resourceRetriever,
+    const std::string& uri,
+    const Eigen::Isometry3d& transform)
+{
+  dart::utils::DartLoader urdfLoader;
+  const SkeletonPtr skeleton = urdfLoader.parseSkeleton(uri, resourceRetriever);
+
+  if (!skeleton)
+    throw std::runtime_error("unable to load '" + uri + "'");
+
+  dynamic_cast<dart::dynamics::FreeJoint*>(skeleton->getJoint(0))
+      ->setTransform(transform);
+  return skeleton;
 }
 
 int main(int argc, char** argv)
@@ -42,6 +61,26 @@ int main(int argc, char** argv)
 
   // Add Human to the viewer.
   viewer.setAutoUpdate(true);
+
+  // Add a loaded table to the scene.
+  const std::string tableURDFUri(
+      "package://pr_assets/data/furniture/uw_demo_table.urdf");
+
+  Eigen::Isometry3d tablePose = Eigen::Isometry3d::Identity();
+  tablePose.translation() = Eigen::Vector3d(1.0, 0.0, 0);
+  Eigen::Matrix3d rot;
+  rot = Eigen::AngleAxisd(M_PI_2, Eigen::Vector3d::UnitZ())
+        * Eigen::AngleAxisd(0, Eigen::Vector3d::UnitY())
+        * Eigen::AngleAxisd(0, Eigen::Vector3d::UnitX());
+  tablePose.linear() = rot;
+
+  // Load table
+  const auto resourceRetriever
+      = std::make_shared<aikido::io::CatkinResourceRetriever>();
+  SkeletonPtr table = makeBodyFromURDF(resourceRetriever, tableURDFUri, tablePose);
+
+  // Add all objects to World
+  human.getWorld()->addSkeleton(table);
 
   waitForUser("Press [ENTER] to exit: ");
 
